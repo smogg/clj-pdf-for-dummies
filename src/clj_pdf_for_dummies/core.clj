1b64debb7bfef6970d39153f6f11ceefa5651dae
Oskar Zabik <oskar.zabik@gmail.com>
2017-12-09 22:36:03 +0100

    add title page

--- src/clj_pdf_for_dummies/core.clj
+++ src/clj_pdf_for_dummies/core.clj
@@ -1,18 +1,95 @@
 (ns clj-pdf-for-dummies.core
   (:require [clj-pdf.core :as pdf]
+            [hiccup.core :refer [html]]
             [clojure.tools.cli :refer [parse-opts]]
             [clojure.java.io :as io])
   (:gen-class))
 
+; -- Fonts ---------------------------------------------------------------------
+; register clj-pdf fonts:
+(clj-pdf.graphics-2d/g2d-register-fonts [["resources/fonts" false]])
+; create java.awt.Font instances for use with [:graphics]:
+(def special-elite-font (java.awt.Font/createFont
+                          java.awt.Font/TRUETYPE_FONT
+                          (-> "fonts/SpecialElite-Regular.ttf" io/resource io/input-stream)))
+
+(def lato-bold-italic (java.awt.Font/createFont
+                        java.awt.Font/TRUETYPE_FONT
+                        (-> "fonts/Lato-BoldItalic.ttf" io/resource io/input-stream)))
+
+(defn resize-font
+ [font size]
+ (.deriveFont font (float size)))
+
+(defn center-font-x
+  [g2d width font string]
+  (let [font-metrics (.getFontMetrics g2d font)]
+    (int (- (/ width 2)
+            (/ (.stringWidth font-metrics string) 2)))))
+
+; -- PDF Sizes -
 (def page-size :letter)
 (def page-width 612)
 (def page-height 792)
 (def margin-left 30)
 (def margin-right 30)
 
+; -- Colors -------------------------------------------------------------------
+(def black "#000000")
+(def white "#ffffff")
+
+; -- Helpers -------------------------------------------------------------------
+(defn svg
+  "Returns a properly formatted SVG string with given attributes and body.
+  Meant to be used inside `clj-pdf`'s `[:svg]` element."
+  [attrs & body]
+  (html
+   (into
+    [:svg (merge {:xmlns "http://www.w3.org/2000/svg"} attrs)]
+    body)))
+
+; -- Pages ---------------------------------------------------------------------
 (defn cover
   [])
 
+(defn draw-string-in-center
+ [g2d text y width]
+ (.drawString g2d 0 0))
+
+(def title-square
+ (let [width 400
+       height 200
+       x (- (/ page-width 2) (/ width 2))
+       y 100]
+   [:graphics {:translate [x y]}
+    (fn [g2d]
+      (let [t1 "clj-pdf"
+            t2 "FOR"
+            t3 "DUMMIES"
+            center (partial center-font-x g2d width)
+            special-elite-16 (resize-font special-elite-font 16)
+            special-elite-50 (resize-font special-elite-font 50)
+            lato-bold-italic-40 (resize-font lato-bold-italic 40)]
+        (doto g2d
+          (.setColor java.awt.Color/BLACK)
+          ; draw rectangle
+          (.fillRect 0 0 width height)
+
+          (.setColor java.awt.Color/WHITE)
+
+          ; draw first string
+          (.setFont lato-bold-italic-40)
+          (.drawString t1 (center lato-bold-italic-40 t1) 50)
+
+          ; draw second string
+          (.setFont special-elite-16)
+          (.drawString t2 (center special-elite-16 t2) 100)
+
+          ; draw third string
+          (.setFont special-elite-50)
+          (.drawString t3 (center special-elite-50 t3) 170))))]))
+
+; -- Rendering -----------------------------------------------------------------
 (defn -main
   "Generates the pdf at specified location (defaults to `output/clj-pdf-for-dummies.pdf`)."
   [& args]
@@ -26,6 +103,8 @@
        :left-margin margin-left
        :right-magin margin-right
        :letterhead (cover)
-       :footer {:start-page 2 :align :center}}]
+       :footer {:start-page 2 :align :center}
+       :register-system-fonts? true}
+      title-square]
      dest)
-    (println "Generated pdf at " dest)))
+    (println "Generated pdf at" dest)))
