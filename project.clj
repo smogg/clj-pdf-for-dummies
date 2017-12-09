2990bfd2b70c30536bbb2731ad0b0db7cd7250ed
Oskar Zabik <oskar.zabik@gmail.com>
2017-12-09 18:08:24 +0100

    basic project setup

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :main clj-pdf-for-dummies.core
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.cli "0.3.5"]
                  [clj-pdf "2.2.30"]])
--- src/clj_pdf_for_dummies/core.clj
+++ src/clj_pdf_for_dummies/core.clj
@@ -1,6 +1,31 @@
-(ns clj-pdf-for-dummies.core)
+(ns clj-pdf-for-dummies.core
+  (:require [clj-pdf.core :as pdf]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.java.io :as io])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def page-size :letter)
+(def page-width 612)
+(def page-height 792)
+(def margin-left 30)
+(def margin-right 30)
+
+(defn cover
+  [])
+
+(defn -main
+  "Generates the pdf at specified location (defaults to `output/clj-pdf-for-dummies.pdf`)."
+  [& args]
+  (let [{{:keys [dest]} :options}
+        (parse-opts args
+                    [["-d" "--dest DEST" "Destination to output the pdf to."
+                      :default "output/clj-pdf-for-dummies.pdf"]])]
+    (io/make-parents dest)
+    (pdf/pdf
+     [{:size page-size
+       :left-margin margin-left
+       :right-magin margin-right
+       :letterhead (cover)
+       :footer {:start-page 2 :align :center}}]
+     dest)
+    (println "Generated pdf at " dest)))
