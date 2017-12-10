63294ccd4ba86ccd1e431edafa0f307035a7d311
Oskar Zabik <oskar.zabik@gmail.com>
2017-12-10 17:02:20 +0100

    styling/formatting

--- src/clj_pdf_for_dummies/core.clj
+++ src/clj_pdf_for_dummies/core.clj
@@ -10,17 +10,19 @@
 ; register clj-pdf fonts:
 (clj-pdf.graphics-2d/g2d-register-fonts [["resources/fonts" false]])
 ; create java.awt.Font instances for use with [:graphics]:
-(def special-elite-font (java.awt.Font/createFont
-                          java.awt.Font/TRUETYPE_FONT
-                          (-> "fonts/SpecialElite-Regular.ttf" io/resource io/input-stream)))
+(def special-elite-font
+  (java.awt.Font/createFont
+   java.awt.Font/TRUETYPE_FONT
+   (-> "fonts/SpecialElite-Regular.ttf" io/resource io/input-stream)))
 
-(def lato-bold-italic (java.awt.Font/createFont
-                        java.awt.Font/TRUETYPE_FONT
-                        (-> "fonts/Lato-BoldItalic.ttf" io/resource io/input-stream)))
+(def lato-bold-italic
+  (java.awt.Font/createFont
+   java.awt.Font/TRUETYPE_FONT
+   (-> "fonts/Lato-BoldItalic.ttf" io/resource io/input-stream)))
 
 (defn resize-font
- [font size]
- (.deriveFont font (float size)))
+  [font size]
+  (.deriveFont font (float size)))
 
 (defn center-font-x
   [g2d width font string]
@@ -28,14 +30,14 @@
     (int (- (/ width 2)
             (/ (.stringWidth font-metrics string) 2)))))
 
-; -- PDF Sizes -
+; -- PDF Sizes -----------------------------------------------------------------
 (def page-size :letter)
 (def page-width 612)
 (def page-height 792)
 (def margin-left 30)
 (def margin-right 30)
 
-; -- Colors -------------------------------------------------------------------
+; -- Colors --------------------------------------------------------------------
 (def black "#000000")
 (def white "#ffffff")
 (def yellow "#FCEE37")
@@ -63,30 +65,29 @@
 
 ; -- Pages ---------------------------------------------------------------------
 (defn cover-text
- [g2d width]
- (let [t1 "clj-pdf"
-       t2 "FOR"
-       t3 "DUMMIES"
-       center (partial center-font-x g2d width)
-       special-elite-16 (resize-font special-elite-font 16)
-       special-elite-50 (resize-font special-elite-font 50)
-       lato-bold-italic-40 (resize-font lato-bold-italic 40)]
-   (doto g2d
-    (.setColor java.awt.Color/WHITE)
+  [g2d width]
+  (let [t1 "clj-pdf"
+        t2 "FOR"
+        t3 "DUMMIES"
+        center (partial center-font-x g2d width)
+        special-elite-16 (resize-font special-elite-font 16)
+        special-elite-50 (resize-font special-elite-font 50)
+        lato-bold-italic-40 (resize-font lato-bold-italic 40)]
+    (doto g2d
+      (.setColor java.awt.Color/WHITE)
     ; draw first string
-    (.setFont lato-bold-italic-40)
-    (.drawString t1 (center lato-bold-italic-40 t1) 50)
+      (.setFont lato-bold-italic-40)
+      (.drawString t1 (center lato-bold-italic-40 t1) 50)
 
     ; draw second string
-    (.setFont special-elite-16)
-    (.drawString t2 (center special-elite-16 t2) 100)
+      (.setFont special-elite-16)
+      (.drawString t2 (center special-elite-16 t2) 100)
 
     ; draw third string
-    (.setFont special-elite-50)
-    (.drawString t3 (center special-elite-50 t3) 170))))
+      (.setFont special-elite-50)
+      (.drawString t3 (center special-elite-50 t3) 170))))
 
-(defn cover
-  []
+(def cover
   [[:graphics {:under true}
     (fn [g2d]
       (doto g2d
@@ -94,20 +95,21 @@
         (.fillRect 0 0 page-width page-height)))]
    [:graphics {:translate [0 100] :rotate -0.05}
     (fn [g2d]
-     (let [width (+ page-width 50)]
-       (doto g2d
-         (.setColor (-> turquoise hex->rgb-int java.awt.Color.))
-         (.setFont (java.awt.Font. "GillSans-SemiBold" java.awt.Font/BOLD 22))
-         (.drawString "Start making PDFs today!" margin-left 0)
-         (.setFont (java.awt.Font. "GillSans-SemiBold" java.awt.Font/PLAIN 14))
-         (.drawString
-          "Your absolutely useless and way too short guide to the awesome clj-pdf Clojure library."
-          margin-left
-          20)
-         (.setColor java.awt.Color/BLACK)
-         (.fillRect -25 35 width 300)
-         (.setColor (-> turquoise hex->rgb-int java.awt.Color.))
-         (.fillRect -25 335 width 5))))]
+      (let [width (+ page-width 50)]
+        (doto g2d
+          (.setColor (-> turquoise hex->rgb-int java.awt.Color.))
+          (.setFont (java.awt.Font. "GillSans-SemiBold" java.awt.Font/BOLD 22))
+          (.drawString "Start making PDFs today!" margin-left 0)
+          (.setFont (java.awt.Font. "GillSans-SemiBold" java.awt.Font/PLAIN 14))
+          (.drawString
+           (str "Your absolutely useless and way too short guide to the awesome"
+                " clj-pdf Clojure library.")
+           margin-left
+           20)
+          (.setColor java.awt.Color/BLACK)
+          (.fillRect -25 35 width 300)
+          (.setColor (-> turquoise hex->rgb-int java.awt.Color.))
+          (.fillRect -25 335 width 5))))]
    [:graphics {:translate [0 185] :rotate -0.05}
     #(cover-text % page-width)]
    [:svg {:scale 0.6
@@ -115,20 +117,19 @@
     (io/resource "img/face.svg")]])
 
 (defn draw-string-in-center
- [g2d text y width]
- (.drawString g2d 0 0))
+  [g2d text y width]
+  (.drawString g2d 0 0))
 
 (def title-square
- (let [width 400
-       height 200
-       x (- (/ page-width 2) (/ width 2))
-       y 100]
-   [:graphics {:translate [x y]}
-    (fn [g2d]
-      (doto g2d
-        (.fillRect 0 0 width height)
-        (cover-text width)))]))
-
+  (let [width 400
+        height 200
+        x (- (/ page-width 2) (/ width 2))
+        y 100]
+    [:graphics {:translate [x y]}
+     (fn [g2d]
+       (doto g2d
+         (.fillRect 0 0 width height)
+         (cover-text width)))]))
 
 ; -- Rendering -----------------------------------------------------------------
 (defn -main
@@ -143,7 +144,7 @@
      [{:size page-size
        :left-margin margin-left
        :right-magin margin-right
-       :letterhead (cover)
+       :letterhead cover
        :footer {:start-page 2
                 :align :center}
        :register-system-fonts? true
